7d240f135fd218cc2f1ce408f743735d0cbdad5a
Taurius <ytaurius159@email.com>
2024-08-22 11:48:59 +0300

    NumberOrderingService implementation for saving a file, using sorting service. +unit test

---
+++ NumeriuUzsakymasApi.Tests/Services/NumberOrderingServiceTests.cs
@@ -0,0 +1,37 @@
+using Moq;
+using NumeriuUzsakymasApi.Services;
+using NumeriuUzsakymasApi.Services.Sorting;
+namespace NumeriuUzsakymasApi.Tests.Services
+{
+    [TestClass]
+    public class NumberOrderingServiceTests
+    {
+        private NumberOrderingService _service;
+        private Mock<ISortingService> _sortingServiceMock;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _sortingServiceMock = new Mock<ISortingService>();
+            _service = new NumberOrderingService(_sortingServiceMock.Object);
+        }
+
+        [TestMethod]
+        public void SaveSortedNumber_SavesSortedNumbers()
+        {
+            // Arrange
+            int[] numbers = new int[] { 5, 3, 8, 1, 2, 9, 4, 7, 6 };
+            int[] sortedNumbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            _sortingServiceMock.Setup(x => x.Sort(numbers)).Returns(sortedNumbers);
+
+            // Act
+            _service.SaveSortedNumber(numbers);
+
+            // Assert
+            _sortingServiceMock.Verify(x => x.Sort(numbers), Times.Once);
+            string expected = "1 2 3 4 5 6 7 8 9";
+            string actual = System.IO.File.ReadAllText("resultatas.txt");
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ NumeriuUzsakymasApi/Services/NumberOrderingService.cs
@@ -0,0 +1,22 @@
+using NumeriuUzsakymasApi.Services.Sorting;
+
+namespace NumeriuUzsakymasApi.Services
+{
+    public class NumberOrderingService
+    {
+        private readonly ISortingService _sortingService;
+        private readonly string _filePath = "resultatas.txt";
+
+        public NumberOrderingService(ISortingService sortingService)
+        {
+            _sortingService = sortingService;
+        }
+
+        public async void SaveSortedNumber(int[] numbers)
+        {
+            var sortedNumbers = _sortingService.Sort(numbers);
+            
+            await System.IO.File.WriteAllTextAsync(_filePath, string.Join(" ", sortedNumbers));
+        }
+    }
+}
